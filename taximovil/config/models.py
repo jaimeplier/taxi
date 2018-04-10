4f407ebb18c1ef241f56751750677799aa3a7109
Jose Salvador Nuño Toledo <salvadorjnt@gmail.com>
2018-04-10 14:32:39 -0500

    modificacion al modelo

--- taximovil/config/models.py
+++ taximovil/config/models.py
@@ -9,7 +9,7 @@ from django.contrib.gis.db import models
 
 # Create your models here.
 class UsuarioManager(BaseUserManager):
-    def create_user(self, email, password, rol, nombre, a_paterno):
+    def create_user(self, email, password, rol, nombre, a_paterno, telefono):
         if not email:
             raise ValueError('El usuario necesita un email')
 
@@ -19,12 +19,13 @@ class UsuarioManager(BaseUserManager):
         user.rol = rol
         user.nombre = nombre
         user.a_paterno = a_paterno
+        user.telefono = telefono
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password, rol, nombre, a_paterno):
-        user = self.create_user(email=email, password=password, rol=Rol(pk=1), nombre=nombre, a_paterno=a_paterno)
+    def create_superuser(self, email, password, rol, nombre, a_paterno, telefono):
+        user = self.create_user(email=email, password=password, rol=Rol(pk=1), nombre=nombre, a_paterno=a_paterno, telefono=telefono)
         user.save(using=self._db)
         return user
 
@@ -119,6 +120,9 @@ class Usuario(AbstractBaseUser):
 
 
 class Cliente(Usuario):
+    rfc = models.CharField(max_length=45)
+    #cp = models.IntegerField()
+    #lada = models.IntegerField()
     customer_token = models.CharField(blank=True, null=True, max_length=512)
 
     def get_full_name(self):
@@ -130,6 +134,14 @@ class Cliente(Usuario):
 
 
 class Chofer(Usuario):
+    foto = models.FileField(db_column='foto', upload_to='asociados/', null=True)
+    numero_licencia = models.CharField(max_length=64)
+    turno = models.CharField(max_length=3)
+    saldo = models.FloatField()
+    direccion = models.ForeignKey('Direccion', models.DO_NOTHING)
+    #taxi = models.ForeignKey('Taxi', models.DO_NOTHING)
+
+
 
     def get_full_name(self):
         return str(self.nombre) + ' ' + str(self.a_paterno) + ' ' + str(self.a_materno)
@@ -157,6 +169,10 @@ class Personal(Usuario):
 
 class Sitio(models.Model):
     nombre = models.CharField(max_length=50)
+    num_espacio = models.IntegerField()
+    pv = models.CharField(max_length=45)
+    #estatus_permiso
+    estatus_sesion = models.IntegerField()
 
     class Meta:
         managed = True
@@ -240,15 +256,6 @@ class Idioma(models.Model):
         db_table = 'idioma'
 
 
-class Estado(models.Model):
-    nombre = models.CharField(max_length=45)
-
-    def __str__(self):
-        return self.nombre
-
-    class Meta:
-        managed = True
-        db_table = 'estado'
 
 
 class Pais(models.Model):
@@ -326,3 +333,199 @@ class TipoServicio(models.Model):
     class Meta:
         managed = True
         db_table = 'tipo_servicio'
+
+class Direccion(models.Model):
+    colonia = models.CharField(max_length=100)
+    calle = models.CharField(max_length=100)
+    numero_interior = models.CharField(max_length=20)
+    numero_exterior = models.CharField(max_length=50)
+    cp = models.CharField(max_length=10)
+
+    latlgn = models.PointField()
+    municipio = models.ForeignKey('Municipio',models.DO_NOTHING)
+
+    @property
+    def latitud(self):
+        """I'm the 'x' property."""
+        return str(self.latlgn.coords[1])
+
+    @property
+    def longitud(self):
+        """I'm the 'x' property."""
+        return str(self.latlgn.coords[0])
+
+    class Meta:
+        managed = True
+        db_table = 'direccion'
+
+class Estado(models.Model):
+    nombre = models.CharField(max_length=45)
+    hora_diferencia = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        managed = True
+        db_table = 'estado'
+
+class Municipio(models.Model):
+    nombre = models.CharField(max_length=45)
+    estado = models.ForeignKey('Estado', models.DO_NOTHING)  # Field name made lowercase.
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        managed = True
+        db_table = 'municipio'
+
+
+
+class Vehiculo(models.Model):
+    placa = models.CharField(max_length=45, unique=True)
+    anio = models.IntegerField()
+    cromatica = models.CharField(max_length=45)
+    estatus =  models.BooleanField(default=True)
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+    #modelo = models.ForeignKey('Modelo', models.DO_NOTHING)
+    #propietario = models.ForeignKey('Propietario', models.DO_NOTHING)
+    economico = models.CharField(max_length=15,unique=True)
+    ciudad = models.CharField(max_length=100)
+    activo = models.BooleanField(default=True)
+
+    class Meta:
+        managed = True
+        db_table = 'vehiculo'
+
+class TipoVehiculo(models.Model):
+    nombre = models.CharField(max_length=45, unique=True)
+    caracteristicas = models.CharField(max_length=200)
+    num_max_pasajeros = models.IntegerField()
+    num_maletas = models.IntegerField()
+
+    class Meta:
+        managed = True
+        db_table = 'tipo_vehiculo'
+
+class Base(models.Model):
+    identificador = models.CharField(max_length=15)
+    numero_espacio = models.IntegerField()
+    direccion = models.ForeignKey('Direccion', models.DO_NOTHING)
+    telefono = models.CharField(max_length=25, unique=True)
+
+    #horario = models.ManyToManyField('Horario', models.DO_NOTHING)
+    #sitio = models.ForeignKey('Sitio', models.DO_NOTHING)
+    #puerta = models.ManyToOneRel('Puerta', models.DO_NOTHING)
+
+    class Meta:
+        managed = True
+        db_table = 'base'
+
+
+class Puerta(models.Model):
+    nombre = models.CharField(max_length=45)
+    comentarios = models.CharField(max_length=150)
+
+    class Meta:
+        managed = True
+        db_table = 'puerta'
+
+class Servicio(models.Model):
+    hora_registro = models.DateTimeField
+    #direccion_usuario = ('Direccion', models.DO_NOTHING)
+    cliente = ('Cliente', models.DO_NOTHING)
+    tipo_servicio = ('TipoServicio', models.DO_NOTHING)
+    vehiculo = ('Vehiculo', models.DO_NOTHING)
+    tiempo_aproximado = models.CharField(max_length=20)
+    sitio = ('Sitio', models.DO_NOTHING)
+    ref_lugar = models.CharField(max_length=200)
+    ref_persona = models.CharField(max_length=200)
+    direccion_destino = ('Direccion', models.DO_NOTHING)
+    distancia = models.CharField(max_length=20)
+    costo = models.CharField(max_length=20)
+    forma_pago = models.ForeignKey(TipoPago, models.DO_NOTHING)
+    tarifa = models.ForeignKey('Tarifa', models.DO_NOTHING)
+    estatus = models.IntegerField()
+
+    class Meta:
+        managed = True
+        db_table = 'servicio'
+
+class Sucursal(models.Model):
+    clave = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=200)
+    coordenadas = models.PointField()
+    direccion = models.CharField(max_length=200)
+    usuario = models.ForeignKey('Usuario', models.DO_NOTHING)
+    empresa = models.ForeignKey('Empresa', models.DO_NOTHING)
+
+    class Meta:
+        managed = True
+        db_table = 'sucursal'
+
+class Zona(models.Model):
+    nombre = models.CharField(max_length=45)
+    centro = models.PointField()
+    radio = models.FloatField()
+
+    class Meta:
+        managed = True
+        db_table = 'zona'
+
+class Tarifa(models.Model):
+    tarifa_base = models.FloatField()
+    costo_minimo = models.FloatField()
+    costo_km = models.FloatField()
+    distancia_max = models.FloatField()
+    incremento_distancia = models.FloatField()
+    costo_minuto = models.FloatField()
+
+    ciudad = models.ForeignKey('Ciudad', models.DO_NOTHING)
+    sucursal = models.ForeignKey('Sucursal', models.DO_NOTHING)
+    zona_origen = models.ForeignKey('Zona', models.DO_NOTHING)
+    #zona_destino = models.ForeignKey('Zona', models.DO_NOTHING)
+    sitio = models.ForeignKey('Sitio', models.DO_NOTHING)
+    base = models.ForeignKey('Base', models.DO_NOTHING)
+    pago = models.ForeignKey('TipoPago', models.DO_NOTHING)
+    tipo_vehiculo = models.ForeignKey('TipoVehiculo', models.DO_NOTHING)
+
+    class Meta:
+        managed = True
+        db_table = 'tarifa'
+
+class Ciudad(models.Model):
+    nombre = models.CharField(max_length=45)
+    estatus = models.BooleanField(default=True)
+    factor_tiempo = models.FloatField()
+    centro = models.PointField()
+    radio = models.IntegerField()
+    pais = models.ForeignKey('Pais', models.DO_NOTHING)
+
+    class Meta:
+        managed = True
+        db_table = 'ciudad'
+
+class Cuentas(models.Model):
+    cie = models.CharField(max_length=7)
+    clabe = models.CharField(max_length=18)
+    #chofer = models.ManyToOneRel('Cuentas', models.DO_NOTHING)
+
+    class Meta:
+        managed = True
+        db_table = 'cuentas'
+
+class Documentos(models.Model):
+    licencia_condicir = models.CharField(max_length=200)
+    licencia_auto = models.CharField(max_length=200)
+    seguro_auto = models.CharField(max_length=200)
+    tarjeta_circulacion = models.CharField(max_length=200)
+    placas = models.CharField(max_length=200, unique=True)
+    verificacion = models.CharField(max_length=200)
+    penales = models.CharField(max_length=200)
+    toxicologico = models.CharField(max_length=200)
+    chofer = ('Chofer', models.DO_NOTHING)
+
+    class Meta:
+        managed = True
+        db_table = 'documentos'
