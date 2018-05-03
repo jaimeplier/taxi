44ff42cb82f3a8a664851f6e6975333617d9aab6
Jose Salvador Nuño Toledo <salvadorjnt@gmail.com>
2018-05-02 19:28:14 -0500

    Registros admin

--- taximovil/config/admin.py
+++ taximovil/config/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from config.models import Chofer, Empresa, Personal, Sitio, Comisiones, Pais, TipoPago, TipoServicio, Estado, Municipio, \
+    Vehiculo, TipoVehiculo, Base, Puerta, Sucursal, Zona, Tarifa, Ciudad, Documentos, Marca, Modelo, Propietario, \
+    Tarjeta
 
 # Register your models here.
+admin.site.register(Chofer)
+admin.site.register(Empresa)
+admin.site.register(Personal)
+admin.site.register(Sitio)
+admin.site.register(Comisiones)
+admin.site.register(Pais)
+admin.site.register(TipoPago)
+admin.site.register(TipoServicio)
+admin.site.register(Estado)
+admin.site.register(Municipio)
+admin.site.register(Vehiculo)
+admin.site.register(TipoVehiculo)
+admin.site.register(Base)
+admin.site.register(Puerta)
+admin.site.register(Sucursal)
+admin.site.register(Zona)
+admin.site.register(Tarifa)
+admin.site.register(Ciudad)
+admin.site.register(Documentos)
+admin.site.register(Marca)
+admin.site.register(Modelo)
+admin.site.register(Propietario)
+admin.site.register(Tarjeta)
